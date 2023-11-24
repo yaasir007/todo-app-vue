76f2cf25db0b1c762a2a3f36c108ca913f512432
yaasir cheekoory <yaasir1997@gmail.com>
2023-11-25 00:39:25 +0400

    updated btns

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -10,10 +10,10 @@ const props = defineProps(['tasks', 'task', 'index'])
               <span class="ms-2" :class="{ 'text-decoration-line-through': task.completed }">{{ task.title }}</span>
             </div>
             <div class="d-flex justify-content-space-between">
-              <button v-if="task.completed" class="btn btn-sm btn-success mx-1" @click="$emit('toggleCompletedItem', task)">undo</button>
+              <button v-if="task.completed" class="btn btn-sm btn-warning mx-1" @click="$emit('toggleCompletedItem', task)">undo</button>
               <button v-else class="btn btn-sm btn-success mx-1" @click="$emit('toggleCompletedItem', task)">Completed</button>
 
-              <button class="btn btn-sm btn-primary mx-1" @click="$emit('editItem', task, index)">Edit</button>
+              <!-- <button class="btn btn-sm btn-primary mx-1" @click="$emit('editItem', task, index)">Edit</button> -->
               <button class="btn btn-sm btn-danger mx-1" @click="$emit('deleteItem', index)">Delete</button>
             </div>
         </div>
@@ -24,7 +24,7 @@ const props = defineProps(['tasks', 'task', 'index'])
               <span class="ms-2" :class="{ 'text-decoration-line-through': task.completed }">{{ task.title }}</span>
             </div>
             <div class="d-flex justify-content-space-between">
-              <button v-if="task.completed" class="btn btn-sm btn-success mx-1" @click="$emit('toggleCompletedItem', task)">undo</button>
+              <button v-if="task.completed" class="btn btn-sm btn-warning mx-1" @click="$emit('toggleCompletedItem', task)">undo</button>
               <button v-else class="btn btn-sm btn-success mx-1" @click="$emit('toggleCompletedItem', task)">Completed</button>
 
               <button class="btn btn-sm btn-primary mx-1" @click="$emit('editItem', task, index)">Edit</button>
